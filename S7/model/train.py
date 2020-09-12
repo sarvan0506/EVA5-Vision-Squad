e1f77cc43ec2a9dd660a750ba6ce9f436ba06dbc
Alagar, Saravana <saravana.alagar@us.fujitsu.com>
2020-09-12 11:57:50 +0530

    added libraries

---
+++ S7/model/train.py
@@ -0,0 +1,38 @@
+from tqdm import tqdm
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+def train(model, device, train_loader, optimizer, epoch, l1_factor):
+    model.train()
+    pbar = tqdm(train_loader)
+    correct = 0
+    processed = 0
+    for batch_idx, (data, target) in enumerate(pbar):
+        # Get samples
+        data, target = data.to(device), target.to(device)
+
+        # Set gradients to zero before starting backpropagation
+        optimizer.zero_grad()
+
+        # Predict output
+        y_pred = model(data)
+
+        # Calculate loss
+        loss = F.nll_loss(y_pred, target)
+        if l1_factor > 0:  # Apply L1 regularization
+            l1_criteria = nn.L1Loss(size_average=False)
+            regularizer_loss = 0
+            for parameter in model.parameters():
+                regularizer_loss += l1_criteria(parameter, torch.zeros_like(parameter))
+            loss += l1_factor * regularizer_loss
+
+        # Perform backpropagation
+        loss.backward()
+        optimizer.step()
+
+        # Update Progress Bar
+        pred = y_pred.argmax(dim=1, keepdim=True)
+        correct += pred.eq(target.view_as(pred)).sum().item()
+        processed += len(data)
+        pbar.set_description(desc=f'Loss={loss.item():0.2f} Batch_ID={batch_idx} Accuracy={(100 * correct / processed):.2f}')
