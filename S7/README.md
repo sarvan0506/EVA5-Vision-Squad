98a7d2035e57ba0746efdecc2d771a0cc2469f5b
root <sarvan0506@gmail.com>
2020-09-12 21:39:36 +0530

    Completed Code S7, S6 rerun

---
+++ S7/README.md
@@ -0,0 +1,4 @@
+## Types of Convolutions
+
+In this exercise we are going to look into the type of convolutions other than the regular convolution and max pooling. In the exercise we are going to use the **CIFAR10** dataset.
+
--- S7/model/train.py
+++ S7/model/train.py
@@ -1,14 +1,25 @@
-from tqdm import tqdm
-import torch
-import torch.nn as nn
 import torch.nn.functional as F
+from tqdm import tqdm
 
-def train(model, device, train_loader, optimizer, epoch, l1_factor):
+from model.regularizer import l1
+
+
+def train(model, loader, device, optimizer, criterion, l1_factor=0.0):
+    """Train the model.
+    Args:
+        model: Model instance.
+        device: Device where the data will be loaded.
+        loader: Training data loader.
+        optimizer: Optimizer for the model.
+        criterion: Loss Function.
+        l1_factor: L1 regularization factor.
+    """
+
     model.train()
-    pbar = tqdm(train_loader)
+    pbar = tqdm(loader)
     correct = 0
     processed = 0
-    for batch_idx, (data, target) in enumerate(pbar):
+    for batch_idx, (data, target) in enumerate(pbar, 0):
         # Get samples
         data, target = data.to(device), target.to(device)
 
@@ -19,13 +30,7 @@ def train(model, device, train_loader, optimizer, epoch, l1_factor):
         y_pred = model(data)
 
         # Calculate loss
-        loss = F.nll_loss(y_pred, target)
-        if l1_factor > 0:  # Apply L1 regularization
-            l1_criteria = nn.L1Loss(size_average=False)
-            regularizer_loss = 0
-            for parameter in model.parameters():
-                regularizer_loss += l1_criteria(parameter, torch.zeros_like(parameter))
-            loss += l1_factor * regularizer_loss
+        loss = l1(model, criterion(y_pred, target), l1_factor)
 
         # Perform backpropagation
         loss.backward()
@@ -35,4 +40,6 @@ def train(model, device, train_loader, optimizer, epoch, l1_factor):
         pred = y_pred.argmax(dim=1, keepdim=True)
         correct += pred.eq(target.view_as(pred)).sum().item()
         processed += len(data)
-        pbar.set_description(desc=f'Loss={loss.item():0.2f} Batch_ID={batch_idx} Accuracy={(100 * correct / processed):.2f}')
+        pbar.set_description(
+            desc=f'Loss={loss.item():0.2f} Batch_ID={batch_idx} Accuracy={(100 * correct / processed):.2f}'
+        )
